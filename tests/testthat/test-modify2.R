45be0ea4e82e30e16d3c820f4b4bab5557c6264a
kangy10 <kangy10@miamioh.edu>
2024-08-03 16:52:35 -0400

    Used SAR in test-modify2

--- tests/testthat/test-modify2.R
+++ tests/testthat/test-modify2.R
@@ -7,49 +7,9 @@ test_that("only accept proper input", {
                "edge can only have entry equal to 0 or 1")
   expect_error(modify2(bandwidth = -1, edge = matrix(1, 4, 4)),
                "bandwidth must be a positive integer")
-  edge <- matrix(0, 9, 9)
-  edge[4:5, ] <- 1
-  edge[8, 8] <- 1
-  expect_no_error(modify2(bandwidth = 1, edge = edge, plot = FALSE))
-  edge <- matrix(0, 9, 9)
-  edge[4:5, ] <- 1
-  edge[8, 8] <- 1
-  expect_no_error(modify2(bandwidth = 3, edge = edge, plot = TRUE))
-  edge <- matrix(0, 9, 9)
-  edge[1, 1] <- 1
-  edge[9, 9] <- 1
-  edge[1, 9] <- 1
-  edge[9, 1] <- 1
-  edge[5, 5] <- 1
-  expect_no_error(modify2(bandwidth = 1, edge = edge, plot = TRUE))
-  edge <- matrix(0, 9, 9)
-  edge[1, 1] <- 1
-  edge[9, 9] <- 1
-  edge[1, 9] <- 1
-  edge[9, 1] <- 1
-  edge[5, 5] <- 1
-  expect_no_error(modify2(bandwidth = 3, edge = edge, plot = TRUE))
-  edge <- matrix(0, 9, 9)
-  edge[1, 5] <- 1
-  edge[9, 5] <- 1
-  edge[5, 9] <- 1
-  edge[5, 1] <- 1
-  edge[5, 5] <- 1
-  expect_no_error(modify2(bandwidth = 2, edge = edge, plot = TRUE))
-  edge <- matrix(0, 9, 9)
-  edge[1, 5] <- 1
-  edge[9, 5] <- 1
-  edge[5, 9] <- 1
-  edge[5, 1] <- 1
-  edge[5, 5] <- 1
-  expect_no_error(modify2(bandwidth = 30, edge = edge, plot = TRUE))
-  # edge <- matrix(0, 20, 20)
-  # edge[2, 5] <- 1
-  # edge[20, 5] <- 1
-  # edge[5, 20] <- 1
-  # edge[5, 2] <- 1
-  # edge[5, 5] <- 1
-  # expect_no_error(modify2(bandwidth = 2, edge = edge, plot = TRUE))
+  stepedge <- stepEdge(image = sar, bandwidth = 10, thresh = 17, degree = 1)
+  expect_no_error(modify2(bandwidth = 10, edge = stepedge, plot = FALSE))
+  expect_no_error(modify2(bandwidth = 10, edge = stepedge, plot = TRUE))
 })
 
 print("This is the end of test-modify2")
