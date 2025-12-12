c12615a7f8f01333fc027209beae925125601b9e
kangy10 <kangy10@miamioh.edu>
2025-12-12 15:49:22 -0500

    Rename the arguments in the function dKQ(): edge1 --> set1, edge2 --> set2

--- R/dKQ.R
+++ R/dKQ.R
@@ -1,21 +1,21 @@
 # This is R source code for function 'dKQ' in the R package
 # 'image'.
 
-dKQ <- function(edge1, edge2){
-  if (!is.matrix(edge1) || !is.matrix(edge2))
+dKQ <- function(set1, set2){
+  if (!is.matrix(set1) || !is.matrix(set2))
     stop('Both arguments must be matrices.')
-  if ((dim(edge1)[1] != dim(edge2)[1]) ||
-      (dim(edge1)[2] != dim(edge2)[2]))
+  if ((dim(set1)[1] != dim(set2)[1]) ||
+      (dim(set1)[2] != dim(set2)[2]))
     stop('Two matrices must be of the same size.')
-  if (dim(edge1)[1] != dim(edge1)[2])
+  if (dim(set1)[1] != dim(set1)[2])
     stop('Argument must be a square matrix.')
-  if ((length(edge1[(edge1 != 0) & (edge1 != 1)]) >= 1) ||
-      (length(edge2[(edge2 != 0) & (edge2 != 1)]) >= 1))
+  if ((length(set1[(set1 != 0) & (set1 != 1)]) >= 1) ||
+      (length(set2[(set2 != 0) & (set2 != 1)]) >= 1))
     stop('Both matrices can only have entries of 0 or 1.')
-  n <- dim(edge1)[1]
+  n <- dim(set1)[1]
   out <- .Fortran(C_d_kq, n = as.integer(n - 1), edge1 =
-    matrix(as.integer(edge1), ncol = n), edge2 =
-    matrix(as.integer(edge2), ncol = n), dKQ = as.double(100))
+    matrix(as.integer(set1), ncol = n), edge2 =
+    matrix(as.integer(set2), ncol = n), dKQ = as.double(100))
   return(out$dKQ)
 }
 
