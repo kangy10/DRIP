f9883c62ada07ee3fbe8c693d04a4022cb3ca0ee
kangy10 <kangy10@miamioh.edu>
2026-08-11 10:10:03 -0400

    Added output test in stepEdge().

--- tests/testthat/test-stepEdge.R
+++ tests/testthat/test-stepEdge.R
@@ -12,4 +12,17 @@ test_that("degree either 0 or 1", {
                            thresh = 17, degree = 1, blur = TRUE))
 })
 
+test_that("output test -- detect step edges as expected", {
+  testImg <- matrix(0, 100, 100)
+  testImg[50:100, ] <- 1
+  edgehat <- stepEdge(image = testImg, bandwidth = 2, thresh = 1, degree = 0, plot = FALSE)
+  edge <- matrix(0, 100, 100)
+  edge[50, ] <- 1
+  res <- dKQ(edge, edgehat)
+  expect_equal(ifelse(abs(res) < 2.0/nrow(testImg), 0, 1), 0) # The Hausdorff distance is bounded by bandwidth
+  edgehat1 <- stepEdge(image = testImg, bandwidth = 3, thresh = 0.3, degree = 1, plot = TRUE)
+  res1 <- dKQ(edge, edgehat1)
+  expect_equal(ifelse(abs(res1) < 3.0/nrow(testImg), 0, 1), 0) # The Hausdorff distance is bounded by bandwidth
+})
+
 print("This is the end of test-stepEdge")
